7e27733e728d7e639913ecbf24367b1afb1f3152
dyuthiabhitha <71492406+dyuthiabhitha@users.noreply.github.com>
2022-07-13 18:57:59 +0530

    Add files via upload

---
+++ pw (1).py
@@ -0,0 +1,525 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[41]:
+
+
+#Install all the Required Libraries and Packages 
+import os
+import glob
+from tqdm import tqdm
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.io import wavfile
+from python_speech_features import mfcc , logfbank
+import librosa as lr
+import os, glob, pickle
+import librosa
+from scipy import signal
+import noisereduce as nr
+from glob import glob
+import librosa
+from IPython import get_ipython
+get_ipython().run_line_magic('matplotlib', 'inline')
+#All the Required Packages and Libraies are installed.
+import soundfile
+from tensorflow.keras.layers import Conv2D,MaxPool2D, Flatten, LSTM
+from keras.layers import Dropout,Dense,TimeDistributed
+from keras.models import Sequential
+from keras.utils import to_categorical 
+from sklearn.utils.class_weight import compute_class_weight
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score
+
+
+# In[2]:
+
+
+#Loading the required RAVDESS DataSet with length of 1439 Audio Files 
+path="C:/Users/dyuth/Downloads/speech-emotion-recognition-ravdess-data"
+os.listdir(path)
+def getListOfFiles(dirName):
+    listOfFile=os.listdir(dirName)
+    allFiles=list()
+    for entry in listOfFile:
+        fullPath=os.path.join(dirName, entry)
+        if os.path.isdir(fullPath):
+            allFiles=allFiles + getListOfFiles(fullPath)
+        else:
+            allFiles.append(fullPath)
+    return allFiles
+
+dirName = 'C:/Users/dyuth/Downloads/speech-emotion-recognition-ravdess-data'
+listOfFiles = getListOfFiles(dirName)
+len(listOfFiles)
+
+
+# In[3]:
+
+
+#Use the Speech-Recognition API to get the Raw Text from Audio Files, Though Speech Recognition
+#is less strong for large chunk of files, so used Error Handling, where when it is not be able to 
+#produce the text of a particular Audio File it prints the statement 'error'.Just for understanding Audio
+import speech_recognition as sr
+r=sr.Recognizer()
+for file in range(0 , len(listOfFiles) , 1):
+    with sr.AudioFile(listOfFiles[file]) as source:
+        audio = r.listen(source)
+        try:
+            text = r.recognize_google(audio)
+            print(text)
+        except:
+            print('error')
+
+
+# In[6]:
+
+
+#Now Cleaning Step is Performed where:
+#DOWN SAMPLING OF AUDIO FILES IS DONE  AND PUT MASK OVER IT AND DIRECT INTO CLEAN FOLDER
+#MASK IS TO REMOVE UNNECESSARY EMPTY VOIVES AROUND THE MAIN AUDIO VOICE 
+def envelope(y , rate, threshold):
+    mask=[]
+    y=pd.Series(y).apply(np.abs)
+    y_mean = y.rolling(window=int(rate/10) ,  min_periods=1 , center = True).mean()
+    for mean in y_mean:
+        if mean>threshold:
+            mask.append(True)
+        else:
+            mask.append(False)
+    return mask
+
+
+# In[7]:
+
+
+#Plotting the Basic Graphs for understanding of Audio Files :
+for file in range(0 , len(listOfFiles) , 1):
+    audio , sfreq = lr.load(listOfFiles[file])
+    time = np.arange(0 , len(audio)) / sfreq
+    
+    fig ,ax = plt.subplots()
+    ax.plot(time , audio)
+    ax.set(xlabel = 'Time (s)' , ylabel = 'Sound Amplitude')
+    plt.show()
+    
+#PLOT THE SPECTOGRAM
+for file in range(0 , len(listOfFiles) , 1):
+     sample_rate , samples = wavfile.read(listOfFiles[file])
+     frequencies , times, spectrogram = signal.spectrogram(samples, sample_rate) 
+     plt.pcolormesh(times, frequencies, spectrogram)
+     plt.imshow(spectrogram)
+     plt.ylabel('Frequency [Hz]')
+     plt.xlabel('Time [sec]')
+     plt.show()
+
+
+# In[18]:
+
+
+#Next Step is In-Depth Visualisation of Audio Fields and its certain features to plot for.
+#They are the Plotting Functions to be called later. 
+def plot_signals(signals):
+    fig , axes = plt.subplots(nrows=2, ncols=5,sharex =False , sharey=True, figsize=(20,5))
+    fig.suptitle('Time Series' , size=16)
+    i=0
+    for x in range(2):
+      for y in range(5):
+         axes[x,y].set_title(list(signals.keys())[i])
+         axes[x,y].plot(list(signals.values())[i])
+         axes[x,y].get_xaxis().set_visible(False)
+         axes[x,y].get_yaxis().set_visible(False)
+         i +=1
+
+def plot_fft(fft):
+    fig , axes = plt.subplots(nrows=2, ncols=5,sharex =False , sharey=True, figsize=(20,5))
+    fig.suptitle('Fourier Transform' , size=16)
+    i=0
+    for x in range(2):
+        for y in range(5):
+            data = list(fft.values())[i]
+            Y,freq = data[0] , data[1]
+            axes[x,y].set_title(list(fft.keys())[i])
+            axes[x,y].plot(freq , Y)
+            axes[x,y].get_xaxis().set_visible(False)
+            axes[x,y].get_yaxis().set_visible(False)
+            i +=1
+    
+def plot_fbank(fbank):
+    fig , axes = plt.subplots(nrows=2, ncols=5,sharex =False , sharey=True, figsize=(20,5))
+    fig.suptitle('Filter Bank Coefficients' , size=16)
+    i=0
+    for x in range(2):
+        for y in range(5):
+            axes[x,y].set_title(list(fbank.keys())[i])
+            axes[x,y].imshow(list(fbank.values())[i],cmap='hot', interpolation = 'nearest')
+            axes[x,y].get_xaxis().set_visible(False)
+            axes[x,y].get_yaxis().set_visible(False)
+            i +=1
+            
+def plot_mfccs(mfccs):
+    fig , axes = plt.subplots(nrows=2, ncols=5,sharex =False , sharey=True, figsize=(20,5))
+    fig.suptitle('Mel Frequency Capstrum  Coefficients' , size=16)
+    i=0
+    for x in range(2):
+        for y in range(5):
+            axes[x,y].set_title(list(signals.keys())[i])
+            axes[x,y].plot(list(signals.values())[i])
+            axes[x,y].get_xaxis().set_visible(False)
+            axes[x,y].get_yaxis().set_visible(False)
+            i +=1
+
+
+def calc_fft(y,rate):
+    n = len(y)
+    freq = np.fft.rfftfreq(n , d= 1/rate)
+    Y= abs(np.fft.rfft(y)/n)
+    return(Y,freq)
+
+
+# In[17]:
+
+
+# Here The Data Set is loaded and plots are Visualised by Calling the Plotting Functions . 
+import matplotlib.pyplot as plt
+from scipy.io import wavfile as wav
+from scipy.fftpack import fft
+import numpy as np
+for file in range(0 , len(listOfFiles) , 1):
+    rate, data = wav.read(listOfFiles[file])
+    fft_out = fft(data)
+    get_ipython().run_line_magic('matplotlib', 'inline')
+    plt.plot(data, np.abs(fft_out))
+    plt.show()
+    
+signals={}
+fft={}
+fbank={}
+mfccs={}
+# load data
+for file in range(0 , len(listOfFiles) , 1):
+#     rate, data = wavfile.read(listOfFiles[file])
+     signal,rate =librosa.load(listOfFiles[file] , sr=44100)
+     mask = envelope(signal , rate , 0.0005)
+     signals[file] = signal
+     fft[file] = calc_fft(signal , rate)
+    
+     bank = logfbank(signal[:rate] , rate , nfilt = 26, nfft = 1103).T
+     fbank[file] = bank
+     mel = mfcc(signal[:rate] , rate , numcep =13 , nfilt = 26 , nfft=1103).T
+     mfccs[file]=mel
+
+plot_signals(signals)
+plt.show()
+
+plot_fft(fft)
+plt.show()
+
+plot_fbank(fbank)
+plt.show()
+
+plot_mfccs(mfccs)
+plt.show
+
+
+# In[18]:
+
+
+#Now Cleaning Step is Performed where:
+#DOWN SAMPLING OF AUDIO FILES IS DONE  AND PUT MASK OVER IT AND DIRECT INTO CLEAN FOLDER
+#MASK IS TO REMOVE UNNECESSARY EMPTY VOIVES AROUND THE MAIN AUDIO VOICE 
+import pandas as pd
+def envelope(y , rate, threshold):
+    mask=[]
+    y=pd.Series(y).apply(np.abs)
+    y_mean = y.rolling(window=int(rate/10) ,  min_periods=1 , center = True).mean()
+    for mean in y_mean:
+        if mean>threshold:
+            mask.append(True)
+        else:
+            mask.append(False)
+    return mask
+
+
+# In[20]:
+
+
+#The clean Audio Files are redirected to Clean Audio Folder Directory 
+from tqdm import tqdm
+from scipy.io import wavfile
+import glob,pickle
+for file in tqdm(glob.glob(r"C:/Users/dyuth/speech-emotion-recognition-ravdess-data//**//*.wav")):
+    file_name = os.path.basename(file)
+    signal , rate = librosa.load(file, sr=16000)
+    mask = envelope(signal,rate, 0.0005)
+    wavfile.write(filename= r"C:/Users/dyuth/clean_speech"+str(file_name), rate=rate,data=signal[mask])
+
+
+# In[39]:
+
+
+#Feature Extraction of Audio Files Function 
+#Extract features (mfcc, chroma, mel) from a sound file
+def extract_feature(file_name, mfcc, chroma, mel):
+    with soundfile.SoundFile(file_name) as sound_file:
+        X = sound_file.read(dtype="float32")
+        sample_rate=sound_file.samplerate
+        if chroma:
+            stft=np.abs(librosa.stft(X))
+        result=np.array([])
+        if mfcc:
+            mfccs=np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=40).T, axis=0)
+        result=np.hstack((result, mfccs))
+        if chroma:
+            chroma=np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T,axis=0)
+        result=np.hstack((result, chroma))
+        if mel:
+            mel=np.mean(librosa.feature.melspectrogram(X, sr=sample_rate).T,axis=0)
+        result=np.hstack((result, mel))
+    return result
+
+
+# In[37]:
+
+
+#Emotions in the RAVDESS dataset to be classified Audio Files based on . 
+emotions={
+  '01':'neutral',
+  '02':'calm',
+  '03':'happy',
+  '04':'sad',
+  '05':'angry',
+  '06':'fearful',
+  '07':'disgust',
+  '08':'surprised'
+}
+#These are the emotions User wants to observe more :
+observed_emotions=['calm', 'happy', 'fearful', 'disgust']
+
+
+# In[43]:
+
+
+#Load the data and extract features for each sound file
+from glob import glob
+import os
+import glob
+x,y=[],[]
+def load_data(test_size=0.33):
+    answer = 0
+    for file in glob.glob(r'C:\Users\dyuth\clean_speech\*.wav'):
+        file_name=os.path.basename(file)
+        emotion=emotions[file_name.split("-")[2]]
+        if emotion not in observed_emotions:
+            answer += 1
+            continue
+        feature=extract_feature(file, mfcc=True, chroma=True, mel=True)
+        x.append(feature)
+        y.append([emotion,file_name])
+    return train_test_split(np.array(x), y,test_size=test_size, random_state=9)
+load_data()
+
+
+# In[44]:
+
+
+x
+
+
+# In[47]:
+
+
+#Split the dataset
+import librosa
+import numpy as np
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_trai,y_tes=train_test_split(x,y,test_size=0.25)
+print(np.shape(x_train),np.shape(x_test), np.shape(y_trai),np.shape(y_tes))
+y_test_map = np.array(y_tes).T
+y_test = y_test_map[0]
+test_filename = y_test_map[1]
+y_train_map = np.array(y_trai).T
+y_train = y_train_map[0]
+train_filename = y_train_map[1]
+print(np.shape(y_train),np.shape(y_test))
+print(*test_filename,sep="\n")
+
+
+# In[52]:
+
+
+#Get the shape of the training and testing datasets
+# print((x_train.shape[0], x_test.shape[0]))
+print((x_train[0], x_test[0]))
+#Get the number of features extracted
+x_train=np.array(x_train)
+print(f'Features extracted: {x_train.shape}')
+
+
+# In[53]:
+
+
+# Initialize the Multi Layer Perceptron Classifier
+model=MLPClassifier(alpha=0.01, batch_size=256, epsilon=1e-08, hidden_layer_sizes=(300,), learning_rate='adaptive', max_iter=500)
+
+
+# In[54]:
+
+
+#Train the model
+model.fit(x_train,y_train)
+
+
+# In[55]:
+
+
+#SAVING THE MODEL
+import pickle
+# Save the Modle to file in the current working directory
+#For any new testing data other than the data in dataset
+
+Pkl_Filename = "Emotion_Voice_Detection_Model.pkl"  
+
+with open(Pkl_Filename, 'wb') as file:  
+    pickle.dump(model, file)
+
+
+# In[56]:
+
+
+# Load the Model back from file
+with open(Pkl_Filename, 'rb') as file:  
+    Emotion_Voice_Detection_Model = pickle.load(file)
+
+Emotion_Voice_Detection_Model
+
+
+# In[57]:
+
+
+#predicting :
+y_pred=Emotion_Voice_Detection_Model.predict(x_test)
+y_pred
+
+
+# In[64]:
+
+
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+results = confusion_matrix(y_test,y_pred)
+print('confusion matrix')
+print(results)
+print('accuracy score :',accuracy_score(y_test,y_test))
+print('Report')
+print(classification_report(y_test,y_test) )
+
+
+# In[65]:
+
+
+#Store the Prediction probabilities into CSV file 
+import numpy as np
+import pandas as pd
+y_pred1 = pd.DataFrame(y_pred, columns=['predictions'])
+y_pred1['file_names'] = test_filename
+print(y_pred1)
+y_pred1.to_csv('predictionfinal.csv')
+
+
+# In[76]:
+
+
+#RECORDED USING MICROPHONE:
+import pyaudio
+import wave
+
+CHUNK = 1024 
+FORMAT = pyaudio.paInt16 #paInt8
+CHANNELS = 2 
+RATE = 44100 #sample rate
+RECORD_SECONDS = 4
+WAVE_OUTPUT_FILENAME = "output10.wav"
+
+p = pyaudio.PyAudio()
+
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK) #buffer
+
+print("* recording")
+frames = []
+
+for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    data = stream.read(CHUNK)
+    frames.append(data) # 2 bytes(16 bits) per channel
+
+print("* done recording")
+
+stream.stop_stream()
+stream.close()
+p.terminate()
+
+wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+wf.setnchannels(CHANNELS)
+wf.setsampwidth(p.get_sample_size(FORMAT))
+wf.setframerate(RATE)
+wf.writeframes(b''.join(frames))
+wf.close()
+
+
+# In[77]:
+
+
+#The file 'output10.wav' in the next cell is the file that was recorded live using the code :
+data, sampling_rate = librosa.load('output10.wav')
+get_ipython().run_line_magic('matplotlib', 'inline')
+import os
+import pandas as pd
+import librosa.display
+import glob 
+
+plt.figure(figsize=(15, 5))
+librosa.display.waveshow(data, sr=sampling_rate)
+
+
+# In[91]:
+
+
+for file in glob.glob(r'C:\Users\dyuth\clean_speech\*.wav'):
+    print(type(file))
+    break
+
+
+# In[93]:
+
+
+## Appying extract_feature function on random file and then loading model to predict the result 
+file = 'output10.wav'
+# print(type(file))
+filename=os.path.basename(file)
+print(filename)
+filearray=[]
+filearray.append(filename)
+# data , sr = librosa.load(file)
+# data = np.array(data)
+ans =[]
+new_feature ,labels  = extract_feature(filearray, mfcc=True, chroma=True, mel=True)
+ans.append(new_feature)
+ans = np.array(ans)
+# data.shape
+#ans=np.expand_dims(ans,axis=0)
+print(ans.shape)
+Emotion_Voice_Detection_Model.predict(ans)
+
+
+# In[ ]:
+
+
+
+
